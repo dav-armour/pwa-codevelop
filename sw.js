61453336eb5641e83a07afa9ef6e83cdd7d66f86
David Armour <15181647+dav-armour@users.noreply.github.com>
2019-11-12 21:45:54 +1100

    Add cache handling part 1

--- sw.js
+++ sw.js
@@ -1,14 +1,42 @@
+const staticCacheName = 'site-static-v1';
+const assetUrls = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/book.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
 // Install event
 self.addEventListener('install', event => {
-  console.log("Service worker installed");
+  // console.log("Service worker installed");
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(cache => {
+        console.log('caching assets');
+        cache.addAll(assetUrls);
+    })
+  )
+
 })
 
 // Activate event
 self.addEventListener('activate', event => {
-  console.log("Service worker activated");
+  // console.log("Service worker activated");
 })
 
 // Fetch event
 self.addEventListener('fetch', event => {
-  console.log("Fetch event", event);
+  // console.log("Fetch event", event);
+  event.respondWith(
+    caches.match(event.request)
+      .then(cahceRes => {
+        return cahceRes || fetch(event.request);
+      })
+  )
 })
